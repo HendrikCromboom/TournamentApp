5498d3ab15892a96fa8f4f788798b7d9941f20ca
HendrikCromboom <hcromboom@gmail.com>
2020-12-14 14:27:07 +0100

    MatchupModel -init

---
+++ TournamentLibrary/MatchupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentLibrary
+{
+    public class MatchupModel
+    {
+
+        public TeamModel TeamCompeting { get; set; }
+
+        public double Score { get; set; }
+
+        public MatchModel ParentMatch { get; set; }
+    }
+}
