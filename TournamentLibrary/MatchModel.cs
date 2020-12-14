dcafd0f6d04d875f51b5a5fdd1e9a9cfa2d858fb
HendrikCromboom <hcromboom@gmail.com>
2020-12-14 14:25:22 +0100

    MatchModel -init

---
+++ TournamentLibrary/MatchModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentLibrary
+{
+    class MatchModel
+    {
+        public List<MatchupModel> Matchups { get; set; } = new List<MatchupModel>();
+
+        public TeamModel Winner { get; set; }
+
+        public int MatchRound { get; set; }
+
+
+
+    }
+}
