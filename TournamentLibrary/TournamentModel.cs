2d09c49a6e7a2ac64e2a5a6bf165705c075ec18c
HendrikCromboom <hcromboom@gmail.com>
2020-12-14 14:20:49 +0100

    TournamentModel -init

---
+++ TournamentLibrary/TournamentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentLibrary
+{
+    public class TournamentModel
+    {
+        public string TournamentName { get; set; }
+
+        public decimal EntryFee { get; set; }
+
+        public List<TeamModel> Teams { get; set; } = new List<TeamModel>();
+
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+
+        public List<List<MatchModel>> Matches { get; set; } = new List<List<MatchModel>>();
+
+    }
+}
