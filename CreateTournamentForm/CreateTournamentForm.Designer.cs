11cd6580d88356e78a7dd8943ccaf285dde185cf
HendrikCromboom <hcromboom@gmail.com>
2020-12-16 09:43:51 +0100

    CreateTournamentForm -init

---
+++ CreateTournamentForm/CreateTournamentForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CreateTournamentForm
+{
+    partial class CreateTournamentForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createTournamentLabel = new System.Windows.Forms.Label();
+            this.tournamentNameLabel = new System.Windows.Forms.Label();
+            this.tournamentNameValue = new System.Windows.Forms.TextBox();
+            this.entryFeeLabel = new System.Windows.Forms.Label();
+            this.entryFeeValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // createTournamentLabel
+            // 
+            this.createTournamentLabel.AutoSize = true;
+            this.createTournamentLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.createTournamentLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.createTournamentLabel.Location = new System.Drawing.Point(13, 9);
+            this.createTournamentLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.createTournamentLabel.Name = "createTournamentLabel";
+            this.createTournamentLabel.Size = new System.Drawing.Size(324, 50);
+            this.createTournamentLabel.TabIndex = 0;
+            this.createTournamentLabel.Text = "Create Tournament";
+            this.createTournamentLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tournamentNameLabel
+            // 
+            this.tournamentNameLabel.AutoSize = true;
+            this.tournamentNameLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tournamentNameLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.tournamentNameLabel.Location = new System.Drawing.Point(26, 74);
+            this.tournamentNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.tournamentNameLabel.Name = "tournamentNameLabel";
+            this.tournamentNameLabel.Size = new System.Drawing.Size(239, 37);
+            this.tournamentNameLabel.TabIndex = 1;
+            this.tournamentNameLabel.Text = "Tournament Name";
+            this.tournamentNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tournamentNameValue
+            // 
+            this.tournamentNameValue.Location = new System.Drawing.Point(39, 128);
+            this.tournamentNameValue.Name = "tournamentNameValue";
+            this.tournamentNameValue.Size = new System.Drawing.Size(211, 21);
+            this.tournamentNameValue.TabIndex = 2;
+            // 
+            // entryFeeLabel
+            // 
+            this.entryFeeLabel.AutoSize = true;
+            this.entryFeeLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.entryFeeLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.entryFeeLabel.Location = new System.Drawing.Point(26, 164);
+            this.entryFeeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.entryFeeLabel.Name = "entryFeeLabel";
+            this.entryFeeLabel.Size = new System.Drawing.Size(125, 37);
+            this.entryFeeLabel.TabIndex = 1;
+            this.entryFeeLabel.Text = "Entry Fee";
+            this.entryFeeLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // entryFeeValue
+            // 
+            this.entryFeeValue.Location = new System.Drawing.Point(158, 180);
+            this.entryFeeValue.Name = "entryFeeValue";
+            this.entryFeeValue.Size = new System.Drawing.Size(92, 21);
+            this.entryFeeValue.TabIndex = 2;
+            this.entryFeeValue.Text = "0";
+            // 
+            // CreateTournamentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(726, 524);
+            this.Controls.Add(this.entryFeeValue);
+            this.Controls.Add(this.entryFeeLabel);
+            this.Controls.Add(this.tournamentNameValue);
+            this.Controls.Add(this.tournamentNameLabel);
+            this.Controls.Add(this.createTournamentLabel);
+            this.Name = "CreateTournamentForm";
+            this.Text = "Create Tournament";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label createTournamentLabel;
+        private System.Windows.Forms.Label tournamentNameLabel;
+        private System.Windows.Forms.TextBox tournamentNameValue;
+        private System.Windows.Forms.Label entryFeeLabel;
+        private System.Windows.Forms.TextBox entryFeeValue;
+    }
+}
+
